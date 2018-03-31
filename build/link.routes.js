9f4b234c94988c1ce6f503f853292e66be156c5a
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-30 19:26:29 -0700

    add decryption handler

--- build/link.routes.js
+++ build/link.routes.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const utils_1 = require("./utils");
+const CryptoJS = require("crypto-js");
 function default_1(db) {
     const router = express.Router();
     router.post("/", (req, res) => {
@@ -17,8 +18,12 @@ function default_1(db) {
             res.status(201).send("OK");
         });
     });
-    router.get("/:accessUrl", (req, res) => {
+    router.post("/:accessUrl", (req, res) => {
         const { accessUrl } = req.params;
+        const { password } = req.body;
+        if (password == null) {
+            return utils_1.handleError(res, "Password is required", 400);
+        }
         db.collection("links").find({ accessUrl }).toArray((err, docs) => {
             if (err) {
                 return utils_1.handleError(res, "Failed to get link: " + err);
@@ -26,7 +31,25 @@ function default_1(db) {
             if (docs.length < 1) {
                 return utils_1.handleError(res, "Link not found.", 404);
             }
-            res.json(docs[0]);
+            const encryptedLink = docs[0];
+            let decryptedText;
+            try {
+                let bytes = CryptoJS.AES.decrypt(encryptedLink.data, password);
+                decryptedText = bytes.toString(CryptoJS.enc.Utf8);
+            }
+            catch (_a) {
+                return utils_1.handleError(res, "Incorrect Password.", 401);
+            }
+            if (decryptedText == "") {
+                return utils_1.handleError(res, "Incorrect Password.", 401);
+            }
+            try {
+                const link = JSON.parse(decryptedText);
+                res.send(link);
+            }
+            catch (_b) {
+                utils_1.handleError(res, `Error Parsing JSON: ${decryptedText}`, 500);
+            }
         });
     });
     return router;
--- src/link.routes.ts
+++ src/link.routes.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import { handleError } from "./utils";
 import { Link, EncryptedLink } from "./link";
+import * as CryptoJS from "crypto-js";
 
 export default function(db) {
 	const router = express.Router();
@@ -21,8 +22,13 @@ export default function(db) {
 		});
 	});
 
-	router.get("/:accessUrl", (req, res) => {
+	router.post("/:accessUrl", (req, res) => {
 		const { accessUrl } = req.params;
+		const { password } = req.body;
+
+		if (password == null) {
+			return handleError(res, "Password is required", 400);
+		}
 
 		db.collection("links").find({ accessUrl }).toArray((err, docs) => {
 			if (err) {
@@ -31,7 +37,23 @@ export default function(db) {
 			if (docs.length < 1) {
 				return handleError(res, "Link not found.", 404);
 			}
-			res.json(docs[0]);
+			const encryptedLink = docs[0];
+			let decryptedText;
+			try {
+				let bytes = CryptoJS.AES.decrypt(encryptedLink.data, password)
+				decryptedText = bytes.toString(CryptoJS.enc.Utf8);
+			} catch {
+				return handleError(res, "Incorrect Password.", 401);
+			}
+			if (decryptedText == "") {
+				return handleError(res, "Incorrect Password.", 401);
+			}
+			try {
+				const link = JSON.parse(decryptedText);
+				res.send(link);
+			} catch {
+				handleError(res, `Error Parsing JSON: ${decryptedText}`, 500);
+			}
 		});
 	});
 
